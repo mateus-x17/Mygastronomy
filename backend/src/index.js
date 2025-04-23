3cb21437e9bf022cde8ea7d0a787d1be16dbecee
mateus <mateusinaciorocha005@gmail.com>
2025-04-22 22:04:04 -0300

    aplicando variaveis de ambiente

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,26 +5,19 @@ import autenticado from "./auth/auth.js"//importa o arquivo auth.js responsavel 
 import usersRouter from "./routes/users.js"
 import platesRouter from "./routes/plates.js"
 import ordersRouter from "./routes/orders.js"
+import { config } from "dotenv"
 
-// import { config } from "dotenv"
-// config() //carrega as variáveis de ambiente do arquivo .env
-// console.log(process.env.MONGO_CONNECTION_STRING);
-// console.log(process.env.MONGO_DB_NAME);
+
+config() //carrega as variáveis de ambiente do arquivo .env
 
 async function main (){
     const hostname = 'localhost' //endereço do servidor
     const port = 3000 //porta que o servidor vai rodar
     const app = express() // instancia o express para criar o servidor
 
-    // const mongoConnetion = await Mongo.connect({
-    //     mongoConnectionString: process.env.MONGO_CONNECTION_STRING,
-    //     mongoDbName: process.env.MONGO_DB_NAME
-    // }) //conecta ao banco de dados chamando o método connect do objeto Mongo
-    // console.log(mongoConnetion) //imprime o resultado da conexão com o banco de dados
-
     const mongoConnetion = await Mongo.connect({
-        mongoConnectionString: "mongodb+srv://mateusUser:naruto741@mygastronomy-cluster.ull2c.mongodb.net/?retryWrites=true&w=majority&appName=mygastronomy-cluster,",
-        mongoDbName: "mygastronomy-cluster"
+        mongoConnectionString: process.env.MONGO_CONNECTION_STRING,
+        mongoDbName: process.env.MONGO_DB_NAME
     }) //conecta ao banco de dados chamando o método connect do objeto Mongo
     console.log(mongoConnetion) //imprime o resultado da conexão com o banco de dados
 
